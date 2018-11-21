6807b9ad512754ee643914894bf04c671cc3ecb2
lawrence.colombo <colombo.lawrence@gmail.com>
2018-11-21 11:09:36 +0200

    Added Nodejs project
    The nodejs project uses the service-key.json file to retrieve a short lived oauth2 token
    that can be used in fcm postman testing

---
+++ nodejs/src/app.js
@@ -0,0 +1,37 @@
+const { google } = require('googleapis');
+const MESSAGING_SCOPE = 'https://www.googleapis.com/auth/firebase.messaging';
+const SCOPES = [MESSAGING_SCOPE];
+
+/**
+ * Get a valid access token.
+ */
+function getAccessToken() {
+    return new Promise(function (resolve, reject) {
+        let key = require('../resources/service-key');
+        let jwtClient = new google.auth.JWT(
+            key.client_email,
+            null,
+            key.private_key,
+            SCOPES,
+            null
+        );
+        jwtClient.authorize(function (err, tokens) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(tokens.access_token);
+        });
+    });
+}
+
+/**
+ * Log valid access token
+ */
+function logToken() {
+    getAccessToken().then(function (accessToken) {
+        console.log("Access Token: " + accessToken)
+    });
+}
+
+logToken();
